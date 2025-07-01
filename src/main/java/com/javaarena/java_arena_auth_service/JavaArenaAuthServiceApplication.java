66fc5304dfef5fb03e8ee3e69ef2eeb0caa1a48e
Fernandiotkd <fernando.vazquez.ruiz@outlook.com>
2025-07-01 16:14:28 -0600

    First commit

---
+++ src/main/java/com/javaarena/java_arena_auth_service/JavaArenaAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javaarena.java_arena_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaArenaAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaArenaAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaarena/java_arena_auth_service/JavaArenaAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaarena.java_arena_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaArenaAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
